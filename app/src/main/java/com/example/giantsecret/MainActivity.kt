d90befc7ff1a03b257d46be4ab72c011d51bf1b8
sanghyoLe <gilsport1@gmail.com>
2022-01-08 16:39:12 +0900

    Create InputUserDataFragment UI

--- app/src/main/java/com/example/giantsecret/MainActivity.kt
+++ app/src/main/java/com/example/giantsecret/MainActivity.kt
@@ -4,31 +4,42 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.giantsecret.databinding.ActivityMainBinding
+
+import com.example.giantsecret.view.InputUserDataFragment
+
+//import com.example.giantsecret.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityMainBinding
+//    private lateinit var binding:ActivityMainBinding
     private lateinit var mViewModel:MviewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_GiantSecret)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        mViewModel = ViewModelProvider(this).get(MviewModel::class.java)
-
-        binding.lifecycleOwner = this
-        binding.viewModel = mViewModel
-
-        mViewModel.showErrorToast.observe(this, Observer {
-            it.getContentIfNotHandled()?.let {
-                Toast.makeText(this,"모든 값을 입력하세요",Toast.LENGTH_SHORT).show()
+        if(savedInstanceState == null){
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<InputUserDataFragment>(R.id.fragment_container_view)
             }
-        })
+        }
+//        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+//        mViewModel = ViewModelProvider(this).get(MviewModel::class.java)
+//
+//        binding.lifecycleOwner = this
+//        binding.viewModel = mViewModel
+//
+//        mViewModel.showErrorToast.observe(this, Observer {
+//            it.getContentIfNotHandled()?.let {
+//                Toast.makeText(this,"모든 값을 입력하세요",Toast.LENGTH_SHORT).show()
+//            }
+//        })
 
 
 
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
 
     }
